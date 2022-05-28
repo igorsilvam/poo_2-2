bb193f8b4780a804ba807816b642f2af106d8678
igor <igoratendimento@outlook.com.br>
2022-05-28 13:20:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace SimpleEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
